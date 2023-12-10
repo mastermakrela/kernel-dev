2df1761dc57940fc3a10426ca368c0ebc7aceeb0
mastermakrela <mastermakrela@gmail.com>
2023-12-10 13:49:33 +0100

    updated startup sctipt to work with gdb

--- qemu-run.sh
+++ qemu-run.sh
@@ -4,25 +4,24 @@
 HDA="-drive file=arch_aarch64.qcow2,format=qcow2"
 KERNEL=./linux/arch/arm64/boot/Image
 
-CMDLINE='root=/dev/vda2 rw console=ttyS0 kgdboc=ttyS1' # kgdbwait'
+CMDLINE='root=/dev/vda2 rw console=ttyAMA0 kgdboc=ttyS0' # kgdbwait'
 
 SHARED="./share"
 VIRTFS+=" --virtfs local,path=${SHARED},mount_tag=share,security_model=passthrough,id=share "
 
 qemu-system-aarch64 \
     -nodefaults  \
+    -nographic \
     -vga none  \
-    -cpu cortex-a72  \
-    -smp cpus=4,sockets=1,cores=4,threads=1  \
-    -M virt,highmem=off  \
-    -drive if=pflash,format=raw,unit=0,file.filename=edk2-aarch64-code.fd,file.locking=off,readonly=on  \
+    -cpu max  \
+    -machine virt  \
     -m 1024  \
      ${HDA} \
      ${VIRTFS} \
-    -device virtio-rng-pci \
-    -serial stdio -serial tcp::1234,server,nowait \
+    -chardev stdio,mux=on,id=char0 \
+    -chardev socket,path=/tmp/qemu_socket.sock,server=on,wait=off,id=gnc0 \
+    -mon chardev=char0,mode=readline \
+    -serial chardev:char0 \
+    -device pci-serial,id=serial0,chardev=gnc0 \
     -kernel "${KERNEL}" \
-    ${INITRD} \
-    -append "${CMDLINE} " \
-    -gdb tcp::8888 \
-    -nographic
+    -append "${CMDLINE} "
